6e6cbb77b3206d5e5230b886f84c95e1e6d0df13
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-10-30 18:01:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 document.getElementById('feedingLog').addEventListener("submit", async function(event){
-    // event.preventDefault();
+    event.preventDefault();
     
     // get selected radio btn
     const selectedOption_1 = document.querySelector('input[name="roommate"]:checked').value;
     
     const selectedOption_2 = document.getElementById('dropdown').value;
     
+    const selectedOption_3 = document.getElementById('grivence').value;
+    
     // update page/content immediately
     document.getElementById("displayContent_1").textContent = selectedOption_1;
     
     document.getElementById("displayContent_2").textContent = selectedOption_2;
+    
+    document.getElementById("displayContent_3").textContent = selectedOption_3;
 });
