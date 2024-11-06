3446d0e4d018fbcef4a5791fec5803074f801756
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-11-05 20:23:21 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,1 @@
-const { Pool } = require('pg');
 
-const pool = new Pool({
-  username = 'doadmin',
-  password = 'AVNS_aWzkNgnWCugyGo59q8Y',
-  host = 'app-94ff3144-07f5-4290-a51a-6f786a35a630-do-user-18113833-0.f.db.ondigitalocean.com',
-  port = 25060,
-  database = 'defaultdb',
-  sslmode = require
-});
-
-module.exports = {
-  query: (text, params) => pool.query(text, params);
-};
